56d8c78a734d77aad3a15d76314c076ce647e003
root <root@TIGER03590.tigeranalytics.local>
2025-06-02 16:22:54 +0530

    FEAT:Added more add and substract methods

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 
 def calculate():
     a, b = 2, 3
+    return add(a,b), substact(a,b)
+
+def add(a, b):
     return a+b
+
+def substact(a, b):
+    return a-b
 
 if "__name__" == "__main__":
     result = calculate()
